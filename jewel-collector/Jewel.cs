aadd0e172d7014cb9c386201d2012e33763517d8
rodrigodc <rodrigo.donato@controlid.com.br>
2023-05-28 21:59:24 -0300

    primeira alteracao

--- jewel-collector/Jewel.cs
+++ jewel-collector/Jewel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//alteração realizada por Rodrigo Cardador
 namespace jewel_collector
 {
     public enum JewelType
