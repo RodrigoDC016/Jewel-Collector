3302132da8abb388403cf98f4ff9eda00a9f00c8
victorkempa <victorkempa@hotmail.com>
2023-05-29 23:05:18 -0300

    Primeira alteração Victor Kemparski

--- jewel-collector/Jewel.cs
+++ jewel-collector/Jewel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+\\ Alteração Victor Kemparski
 namespace jewel_collector
 {
     public enum JewelType
